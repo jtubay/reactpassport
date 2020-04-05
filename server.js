9820aadc63e06307e253153d0606aa85dfbf45e0
Jean Tubay <jean.tubay.92@gmail.com>
2020-04-05 17:31:15 -0400

    adding dev dependencies and npm packages

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const passport = require('passport')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser');
 const path = require('path');
 const PORT =process.env.PORT || 3001;
 const app = express();
 
-if(process.env.PORT.NODE_ENV === 'production'){
+if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
 }
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize());
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './client/build/index.html'))
