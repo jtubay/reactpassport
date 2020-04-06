0b1863b07cbe12ec83a5f2c5680074bc475ff873
Jean Tubay <jean.tubay.92@gmail.com>
2020-04-05 22:11:16 -0400

    adding to db

--- server.js
+++ server.js
@@ -1,51 +1,55 @@
-const express = require('express');
-// const Sequelize = require('sequelize')
-const logger = require('morgan')
-const bodyParser = require('body-parser')
-// const cookieParser = require('cookie-parser');
-const path = require('path');
-const passport = require('./utils/passport')
-const db = require('./models')
+const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const path = require("path");
+const passport = require("./utils/passport");
+const db = require("./models");
 
-const PORT =process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-
 //middleware
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// app.use(cookieParser());
-// app.use(require('express-session')({
-//     secret: 'keyboard cat',
-//     resave: false,
-//     saveUninitialized: false
-// }))
 app.use(passport.initialize());
-// app.use(passport.session());
 
-//config
-// passport.use(db.User.createStrategy())
-// passport.serializeUser(db.User.serializeUser());
-// passport.deserializeUser(db.User.deserializeUser())
+app.post('/login', (req, res) => {
+    const { email, password } = req.body
+    db.User.findOne({
+        email
+    }, (err, user) => {
+        if(err) throw err;
+        if(!user)
+            return res.status(401).json({success: false, msg: 'Authentication failed.'})
 
-
-app.get('/api/test', passport.authenticate('jwt'), (req, res) => {
-    res.send('Its working')
-})
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build/index.html'))
-})
-
-db.sequelize.sync({ force: true })
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log(`=======> server now on port ${PORT}`)
-        })
+        if(password === user.password) {
+            const token = jwt.sign(user.toJSON(), settings.secret);
+            res.json({success: true, token: 'JWT' + token})
+        } else {
+            res.status(401).send({success: false, msg: 'Authentication failed. wrong pw'})
+        }
     })
-    .catch(err => console.log(err));
+})
+
+//API ROUTES
+app.get("/api/test", passport.authenticate("jwt", { session:false }), (req, res) => {
+  res.send("Its working");
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+db.sequelize
+  .sync({ force: true })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`=======> server now on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
