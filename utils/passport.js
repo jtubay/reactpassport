7fbe9fd24ccc5ce9c8527cc120a596f3a4500311
Jean Tubay <jean.tubay.92@gmail.com>
2020-04-05 23:46:01 -0400

    saving changes

--- utils/passport.js
+++ utils/passport.js
@@ -9,14 +9,15 @@ const JWT_SECRET = process.env.CHAT_JWT_SECRET;
 passport.use(
   new JwtStrategy({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: JWT_SECRET
+      secretOrKey: JWT_SECRET,
     //   issuer: 'accounts.examplesoft.com',
     //   audience: 'yoursite.net'
   },
-  (jwt_payload, done) => {
+  function (jwt_payload, done){
    db.User.findOne({ where: { id: jwt_payload.id }})
     .then(user => {
         if(!user) return done(null, false);
+        
         return done(null, user)
     })
     .catch(err => done(err, false))
