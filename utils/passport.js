236ecc618cd71213d89d0543969d1adb068bdd70
Jean Tubay <jean.tubay.92@gmail.com>
2020-04-05 23:41:05 -0400

    testing

--- utils/passport.js
+++ utils/passport.js
@@ -4,24 +4,22 @@ const ExtractJwt = require("passport-jwt").ExtractJwt;
 
 const db = require('../models')
 
+const JWT_SECRET = process.env.CHAT_JWT_SECRET;
+
 passport.use(
   new JwtStrategy({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'secret',
-      issuer: 'accounts.examplesoft.com',
-      audience: 'yoursite.net'
+      secretOrKey: JWT_SECRET
+    //   issuer: 'accounts.examplesoft.com',
+    //   audience: 'yoursite.net'
   },
   (jwt_payload, done) => {
-    db.User.findOne({ id: jwt_payload.sub }, (err, user) => {
-      if (err) {
-        return done(err, false);
-      }
-      if (user) {
-        return done(null, user);
-      } else {
-        return done(null, false);
-      }
-    });
+   db.User.findOne({ where: { id: jwt_payload.id }})
+    .then(user => {
+        if(!user) return done(null, false);
+        return done(null, user)
+    })
+    .catch(err => done(err, false))
   })
 );
 
